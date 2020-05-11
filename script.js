e90146cd69cd04a60397da088d48c74ca05c36ae
Colin Malcolm <colinmalcolm8@gmail.com>
2020-05-11 18:12:24 -0400

    fixed API, now rendering data

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 
-const url = 'http://newsapi.org/v2/top-headlines?country=us&apiKey=1bd549b086404134a14c4fa7d447f748'
-
-
-const DOMAIN = 'http://newsapi.org/';
+const DOMAIN = 'http://newsapi.org/v2/top-headlines?country=us&';
 const API_KEY = '1bd549b086404134a14c4fa7d447f748';
-const BASE_URL = `${DOMAIN}?apikey=${API_KEY}&s=`;
+const BASE_URL = `https://cors-anywhere.herokuapp.com/${DOMAIN}apikey=${API_KEY}&s=`;
 
 
 let input = document.querySelector(`#find`)
@@ -20,22 +17,22 @@ button.addEventListener('click', (e) => {
 });
 
 async function getSearch(iValue) {
-    try {
+    // try {
         let newsData = await axios.get(BASE_URL + iValue)
-        // console.log(newsData.data.search);
+        console.log(newsData);
 
         for(let i = 0; i < 4; i++) {
-            let newsTitle = newsData.data.Search[i].title 
-            let newsImage = newsData.data.Search[i].urlToImage
-            // console.log(newsTitle, newsImage)
+            let newsTitle = newsData.data.articles[i].title 
+            let newsImage = newsData.data.articles[i].urlToImage
+            console.log(newsTitle, newsImage)
         }
         
 
-    } catch (error) {
+    // } catch (error) {
         
-        console.log(`This is not the news you are looking for. ${error}`)
+    //     console.log(`This is not the news you are looking for. ${error}`)
 
-    }
+    // }
 }
 
 
