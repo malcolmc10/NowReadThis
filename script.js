0a1051008ea28a4ce6f4a992415cc8c7336a60a9
Colin Malcolm <colinmalcolm8@gmail.com>
2020-05-12 12:16:05 -0400

    articles and titles are now showing on site

--- script.js
+++ script.js
@@ -4,57 +4,60 @@ const API_KEY = '1bd549b086404134a14c4fa7d447f748';
 const BASE_URL = `https://cors-anywhere.herokuapp.com/${DOMAIN}apikey=${API_KEY}&s=`;
 
 
+let articleList = document.querySelector('#listArticles')
+
 let input = document.querySelector(`#find`)
+// console.log()
+let form = document.querySelector(`form`)
+console.log(form)
+form.addEventListener('click', (e) => {
+    e.preventDefault()
 
-let button = document.querySelector(`#search`)
-
-button.addEventListener('click', (e) => {
-
-    let inputValue = input.value
+    // console.log("sfsgdfg")
+    let inputValue = input.value;
 
     getSearch(inputValue);
 
 });
 
+
+
 async function getSearch(iValue) {
     // try {
-        let newsData = await axios.get(BASE_URL + iValue)
-        console.log(newsData);
+    let response = await axios.get(BASE_URL + iValue)
+    // console.log(newsData);
+    let newsData = response.data.articles
+    console.log(newsData)
 
-        for(let i = 0; i < 4; i++) {
-            let newsTitle = newsData.data.articles[i].title 
-            let newsImage = newsData.data.articles[i].urlToImage
-            console.log(newsTitle, newsImage)
-        }
-        
+    for (let i = 0; i < 4; i++) {
+        // let newsTitle = newsData.data.articles[i].title 
+        // let newsImage = newsData.data.articles[i].urlToImage
+        // console.log(newsTitle, newsImage)
+
+        renderList(newsData[i])
+    }
+
 
     // } catch (error) {
-        
+
     //     console.log(`This is not the news you are looking for. ${error}`)
 
     // }
 }
 
 
-// function renderList(article) {
 
-//     article.forEach((newArticle) => {
+function renderList(article) {
+    // console.log(article)
+    const title = document.createElement('p')
+    title.innerText = article.title
+    articleList.appendChild(title)
+    console.log(articleList)
 
-//         const appendArticle = document.querySelector('.listArticle')
-
-//         const img = document.createElement('img')
-
-//         const title = document.createElement('p')
-
-//         dataView.classList.add()
-//     })
-
-// }
+    const image = document.createElement('img')
+    image.src = article.urlToImage
+    articleList.appendChild(image)
+    console.log()
 
 
-
-
-
-
-
-
+}
